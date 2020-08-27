926819717eb84b3bd84294736d1382ec95e2bcea
hanodims <hend--mohammed@outlook.com>
2020-08-27 17:30:34 +0300

    new

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,21 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+        'my_list' : [
+            {
+                'restaurant_name': 'Bateel',
+                'food_type':'Coffee & dessert',
+            },
+            {
+                'restaurant_name': 'KFC',
+                'food_type':'Fast Food',
+            },
+             
+            {
+                'restaurant_name': 'Nozomi',
+                'food_type':'Sushi',
+            }
+        ]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +28,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+        'my_object' :  {
+                'restaurant_name': 'Salt',
+                'food_type':'Burger',
+            }
 
     }
     return render(request, 'detail.html', context)
